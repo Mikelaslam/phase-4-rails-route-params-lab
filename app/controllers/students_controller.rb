f6402bf6792e8b3f24a89efcf89875bec234db12
MIKEL DAVIS <kipkorir385@gmail.com>
2023-03-14 04:53:53 +0300

    Completed task

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,20 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+
+  def show
+  student = Student.find_by(id: params[:id])
+  if student
+    render json: {
+      id: student.id,
+      first_name: student.first_name,
+      last_name: student.last_name,
+      grade: student.grade
+    }, status: :ok
+  else
+    render json: { error: 'Student not found' }, status: :not_found
+  end
+end
+
+
 end
